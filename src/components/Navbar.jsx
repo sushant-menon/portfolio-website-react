2787a3863ca3b7294bd154b23fd73c6153510735
Sushant <sushantnambiar@gmail.com>
2023-04-03 02:31:05 +0530

    added latetst version of tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between mx-auto bg-cyan-500 p-4 px-6 md:px-16">
+    <div className="flex justify-between mx-auto bg-cyan-400 p-4 px-6 md:px-16">
       <Link
         to="/home"
         className="text-gray-900 text-3xl cursor-pointer font-extrabold"
