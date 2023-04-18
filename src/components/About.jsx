75224005ff5cd18b3405551e2b0de4a5c3d6cb2b
Sushant <sushantnambiar@gmail.com>
2023-04-18 23:50:41 +0530

    modified about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,17 @@ import AboutMeDetails from "./AboutMeDetails";
 const About = () => {
   return (
     <>
-      <div className="">
+      <div>
         <img
-          className="absolute h-screen object-cover md:w-full blur-sm"
+          className=" absolute h-screen md:w-full object-cover blur-sm"
           src="src\assets\sunset1.jpg"
           alt="bg-image"
         />
-      </div>
-      <div className=" backdrop-blur-sm bg-white/30 relative text-center mt-4 w-[430px] mx-auto rounded-2xl h-full py-6 px-4 md:w-[1370px] md:backdrop-blur-md md:px-16 md:py-12">
-        <AboutMeDetails />
+        <div className="h-screen overflow-y-auto">
+          <div className=" backdrop-blur-sm bg-white/30 relative text-center w-[430px] mx-auto rounded-2xl h-auto py-6 px-4 md:w-[1370px] md:backdrop-blur-md md:px-16 md:py-12 md:mt-20 mt-10">
+            <AboutMeDetails />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/AboutMeDetails.jsx
+++ src/components/AboutMeDetails.jsx
@@ -6,8 +6,8 @@ const AboutMeDetails = () => {
       <div>
         <h1 className="font-extrabold text-4xl">About Me</h1>
       </div>
-      <div className="flex flex-col md:flex-row-reverse mt-12 md:mt-16 px-3 mx-auto justify-center items-center">
-        <span className=" flex justify-center items-center  cursor-pointer group-hover:transform hover:scale-95 duration-1000">
+      <div className="flex flex-col md:flex-row-reverse mt-10 md:mt-16 px-3 mx-auto justify-center items-center md:justify-between">
+        <span className=" flex justify-center items-center cursor-pointer group-hover:transform hover:scale-95 duration-1000">
           <span className="border-2 border-green-950 w-[290px] h-[290px] rounded-md"></span>
           <img
             className="bg-black w-[290px] h-[290px] absolute -translate-x-3 -translate-y-3 rounded-md"
@@ -15,15 +15,27 @@ const AboutMeDetails = () => {
             src="src\assets\Profile.png"
           />
         </span>
-        <p className="mt-3 font-bold">
-          Hey stranger, my name is Sushant Menon. I'm a self taught developer. I
-          work mostly on frontend and for which I use technologies like HTML,
-          CSS, Javascript and React. I started exploring about web development
-          in my final year of Engineering and then started tweaking with html
-          and css and made quite a few static website which made me fall in love
-          with the frontend and that day I chose to move forward with this and
-          pursue this as my career.
-        </p>
+
+        {/* About me */}
+        <div className="flex flex-col">
+          <p className="mt-4 font-bold text-justify md:text-2xl md:max-w-3xl text-lg">
+            Hey visitor, my name is Sushant Menon. I'm a self taught developer.
+            I work mostly on frontend and for which I use technologies like
+            HTML, CSS, Javascript and React. I started exploring about web
+            development in my final year of engineering. Then I began
+            experimenting with HTML and CSS and made quite a few static website
+            which made me fall in love with the frontend and that day I decided
+            to pursue software engineering as a career.
+          </p>
+          {/* Education */}
+          <div className="md:mt-20 mt-10 flex flex-col">
+            <h2 className="text-start font-extrabold text-3xl">Education :</h2>
+            <p className=" text-xl text-start">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+              voluptatibus.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
