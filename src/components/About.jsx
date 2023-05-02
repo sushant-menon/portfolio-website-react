f2c95a23b503742cb969867e2acf902c7e5dd2c8
Sushant <sushantnambiar@gmail.com>
2023-05-03 00:25:43 +0530

    modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         alt="bg-image"
       /> */}
       <div className="h-screen bg-[hsl(42,27%,19%)] overflow-y-auto">
-        <div className="backdrop-blur-sm bg-[#fcf5ea]  relative text-center w-[21rem] mb-5 mx-auto rounded-2xl h-auto py-6 px-4 md:w-[1370px] md:backdrop-blur-md md:px-16 md:py-12 md:mt-20 mt-10">
+        <div className="backdrop-blur-sm bg-[#fcf5ea]  relative text-center w-[20rem] mb-5 mx-auto rounded-2xl h-auto py-6 px-4 md:w-[1370px] md:backdrop-blur-md md:px-16 md:py-12 md:mt-20 mt-10">
           <AboutMeDetails />
         </div>
       </div>
